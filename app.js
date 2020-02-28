10ec08f6679c4839de69096656260be2f76c7f60
Jake Rhoads <jake.rhoads@gmail.com>
2020-02-28 17:15:02 -0500

    recent changes

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-        var loCase = "abcdefghijklmnopqrstuvwxyz";
-        var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var nums = "0123456789";
-        var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-        var loupCase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var loNums = "abcdefghijklmnopqrstuvwxyz0123456789";
+        var specialCharac = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        var lowerUpperCase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var lowerNums = "abcdefghijklmnopqrstuvwxyz0123456789";
         var loSpecial = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
         var upNums = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         var upSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
         var numSpecial = "0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
         var loUpNums = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         var loUpSpecial = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-        var loNumsSpecial = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        var lowerNumsSpecial = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
         var upNumsSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
         var allChar = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>0123456789?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var usersLength = "";
-        var pass = "";
+        var userLength = "";
+        var password = "";
     
     function genPassword(){
         
@@ -55,86 +55,90 @@
         
     if (userChoices1 === true && userChoices2 === false && userChoices3 === false && userChoices4 === false) {       
         for (var i = 0; i < userLength; i++) {
-            pass += loCase.charAt(Math.floor(Math.random() * Math.floor(loCase.length - 1)));
+            pass += lowerCase.charAt(Math.floor(Math.random() * Math.floor(lowerCase.length - 1)));
             
         }
     }
     else if (userChoices2 === true && userChoices1 === false && userChoices3 === false && userChoices4 === false){
         for(var i = 0; i < userLength; i++) {
-            pass += upCase.charAt(Math.floor(Math.random() * Math.floor(upCase.length - 1)));
+            password += upperCase.charAt(Math.floor(Math.random() * Math.floor(upperCase.length - 1)));
         }
     }
     else if (userChoices3 === true && userChoices1 === false && userChoices2 === false && userChoices4 === false) {
         for (var i = 0; i < userLength; i++) {
-            pass += nums.charAt(Math.floor(Math.random() * Math.floor(nums.length - 1)));
+            password += nums.charAt(Math.floor(Math.random() * Math.floor(nums.length - 1)));
         }
     }
     else if (userChoices4 === true && userChoices1 === false && userChoices3 === false && userChoices2 === false){
         for(var i = 0; i < userLength; i++) {
-            pass += specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length - 1)));
+            password += specialCharac.charAt(Math.floor(Math.random() * Math.floor(specialCharac.length - 1)));
         }
     }
     else if (userChoices1 === true && userChoices2 === true && userChoices3 === false && userChoices4 === false) {
         for(var i = 0; i < userLength; i++) {
-            pass += loupCase.charAt(Math.floor(Math.random() * Math.floor(loupCase.length - 1)));
+            password += lowerUpperCase.charAt(Math.floor(Math.random() * Math.floor(lowerUpperCase.length - 1)));
         }
     }
     else if (userChoices1 === true && userChoices3 === true && userChoices2 === false && userChoices4 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += loNums.charAt(Math.floor(Math.random() * Math.floor(loNums.length - 1)));
+                password += lowerNums.charAt(Math.floor(Math.random() * Math.floor(lowerNums.length - 1)));
             }
         }
         else if (userChoices1 === true && userChoices4 === true && userChoices3 === false && userChoices2 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += loSpecial.charAt(Math.floor(Math.random() * Math.floor(loSpecial.length - 1)));
+                password += loSpecial.charAt(Math.floor(Math.random() * Math.floor(loSpecial.length - 1)));
             }
         }
         else if (userChoices2 === true && userChoices3 === true && userChoices1 === false && userChoices2 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += upNums.charAt(Math.floor(Math.random() * Math.floor(upNums.length - 1)));
+                password += upNums.charAt(Math.floor(Math.random() * Math.floor(upNums.length - 1)));
             }
         }
         else if (userChoices2 === true && userChoices4 === true && userChoices1 === false && userChoices3 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += upSpecial.charAt(Math.floor(Math.random() * Math.floor(upSpecial.length - 1)));
+                password += upSpecial.charAt(Math.floor(Math.random() * Math.floor(upSpecial.length - 1)));
             }
         }
         else if (userChoices3 === true && userChoices4 === true && userChoices1 === false && userChoices2 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += numSpecial.charAt(Math.floor(Math.random() * Math.floor(numSpecial.length - 1)));
+                password += numSpecial.charAt(Math.floor(Math.random() * Math.floor(numSpecial.length - 1)));
             }
         }
         else if (userChoices1 === true && userChoices2 === true && userChoices3 === true && userChoices4 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += loUpNums.charAt(Math.floor(Math.random() * Math.floor(loUpNums.length - 1)));
+                password += loUpNums.charAt(Math.floor(Math.random() * Math.floor(loUpNums.length - 1)));
             }
         }
         else if (userChoices1 === true && userChoices2 === true && userChoices4 === true && userChoices3 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += loUpSpecial.charAt(Math.floor(Math.random() * Math.floor(loUpSpecial.length - 1)));
+                password += loUpSpecial.charAt(Math.floor(Math.random() * Math.floor(loUpSpecial.length - 1)));
             }
         }
         else if (userChoices1 === true && userChoices3 === true && userChoices4 === true && userChoices2 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += loNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(loNumsSpecial.length - 1)));
+                password += loNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(loNumsSpecial.length - 1)));
             }
         }
         else if (userChoices2 === true && userChoices3 === true && userChoices4 === true && userChoices1 === false) {
             for(var i = 0; i < userLength; i++) {
-                pass += upNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(upNumsSpecial.length - 1)));
+                password += upNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(upNumsSpecial.length - 1)));
             }
         }
         else if (userChoices2 === true && userChoices3 === true && userChoices4 === true && userChoices1 === true) {
             for(var i = 0; i < userLength; i++) {
-                pass += allChar.charAt(Math.floor(Math.random() * Math.floor(allChar.length - 1)));
+                password += allChar.charAt(Math.floor(Math.random() * Math.floor(allChar.length - 1)));
             }
         } else {
             alert("You must choose at least 1 of the 4 required fields");
         }
     
-    document.getElementById("passOutput").value = pass;
+    document.getElementById("newPassword").value = password;
 
     }
+
+    function myFunction() {
+        document.getElementById("newPassword").reset();
+      }
             
 }
 
